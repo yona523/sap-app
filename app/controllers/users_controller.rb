3f7cc2242915943e296b522c07047144c2675de0
kouhei.yoyo <yona.478.621@gmail.com>
2021-04-09 06:01:52 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def index
+    #User.order(id: :desc)で降順で一覧表示、page(params[:page]).per(20)で１ページにつき25件取得
+    @users = User.order(id: :desc).page(params[:page]).per(20)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+
+  def user_params
+    #パスワードを暗号化するため
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
